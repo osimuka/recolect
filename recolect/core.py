57b772772424fda98462eeb22d7d354220a82e0d
Uka Osim <hsojo91@gmail.com>
2023-10-30 12:53:03 +0000

    updated method with kmeans clustering and comsine-sim

--- recolect/core.py
+++ recolect/core.py
@@ -7,6 +7,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import ENGLISH_STOP_WORDS
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+from nltk.tokenize import word_tokenize
 from nltk.stem import PorterStemmer
 from gensim.models.fasttext import FastText as FT_gensim
 
@@ -19,26 +20,31 @@ def load_data(filepath: str) -> pd.DataFrame:
     return pd.read_csv(filepath)
 
 
+# Precompiled patterns
+NON_WORD_PATTERN = re.compile(r'\W')
+SINGLE_CHAR_PATTERN = re.compile(r'\s+[a-z]\s+')
+
+
 def preprocess_text(text: str) -> str:
-    text = text.lower()  # Convert to lowercase
+    # Convert to lowercase
+    text = text.lower()
+
     # Remove all non-word characters & all single characters
-    text = re.sub(r'\W', ' ', re.sub(r'\s+[a-z]\s+', ' ', text))
-    text = ' '.join([PorterStemmer().stem(word) for word in text.split() if word not in ENGLISH_STOP_WORDS])
-    return text
+    text = NON_WORD_PATTERN.sub(' ', SINGLE_CHAR_PATTERN.sub(' ', text))
+
+    # Tokenization
+    tokens = word_tokenize(text)
+
+    # Stemming
+    stemmer = PorterStemmer()
+    stemmed_tokens = [stemmer.stem(word) for word in tokens if word not in ENGLISH_STOP_WORDS]
+
+    return ' '.join(stemmed_tokens)
 
 
 def load_and_preprocess_data(filepath: str, col: str = None) -> pd.DataFrame:
     data = load_data(filepath)
     data['processed_text'] = data[col or 'description'].apply(lambda x: preprocess_text(x))
-    return data
-
-
-def k_means_clustering(model: typing.Any, data: pd.DataFrame) -> pd.DataFrame:
-    """Train k-means clustering with feature vector Add cluster_id on dataframe"""
-    ft_vectors = [model.wv[word] for word in model.wv.vocab]
-    ft_vectors = np.array(ft_vectors)
-    kmeans = KMeans(n_clusters=50, random_state=42).fit(ft_vectors)
-    data['cluster_id'] = kmeans.predict(ft_vectors)
     return data
 
 
@@ -51,16 +57,68 @@ def train(data: pd.DataFrame, col: str = None) -> pd.DataFrame:
     return model, test
 
 
+def get_avg_vector(words, model):
+    """Get average vector for a list of words"""
+    # Get the vectors for each word in the list
+    vectors = [model.wv[word] for word in words if word in model.wv]
+
+    # If no words are in the model, return a vector of zeros
+    if not vectors:
+        return np.zeros(model.vector_size)
+
+    # Return the average vector
+    return np.mean(vectors, axis=0)
+
+
+def k_means_clustering(model: typing.Any, data: pd.DataFrame, column_name: str) -> pd.DataFrame:
+    """Perform KMeans clustering on the average vectors"""
+
+    # Calculate average feature vectors for the words in each row of the DataFrame
+    data['avg_vector'] = data[column_name].apply(lambda x: get_avg_vector(x, model))
+
+    # Create a matrix of the average vectors
+    avg_vectors_matrix = np.vstack(data['avg_vector'].to_numpy())
+
+    # Fit the KMeans clustering
+    kmeans = KMeans(n_clusters=20, random_state=42, n_init="auto").fit(avg_vectors_matrix)
+
+    # Predict clusters for those average vectors
+    data['cluster_id'] = kmeans.predict(avg_vectors_matrix)
+
+    return data
+
+
 def _get_recommendations_k_means_clustering(title: str, model: typing.Any, data: pd.DataFrame, n: int = 10) -> pd.DataFrame:
     """Get recommendations for a title"""
 
-    data = k_means_clustering(model, data)
     title_row = data[data["title"] == title].copy()
-    result_df = data[data["cluster_id"].isin(title_row["cluster_id"])].copy()
+
+    # Check if title_row is empty (title not found)
+    if title_row.empty:
+        return pd.DataFrame(columns=['title', 'similarity'], data=[])
+
+    # Assign clusters to each row in the data
+    data = k_means_clustering(model, data, "processed_text")
+
+    title_row = data[data["title"] == title].copy()
+
+    # Get the cluster of the given title
+    title_cluster = title_row["cluster_id"].iloc[0]
+
+    # Filter the DataFrame to only include rows from the same cluster as the given title
+    result_df = data[data["cluster_id"] == title_cluster].copy()
+
+    # Exclude the given title itself from the recommendations
     result_df = result_df.drop(result_df[result_df["title"] == title].index)
-    result_df["similarity"] = result_df.apply(lambda x: model.wv.n_similarity(title_row["processed_text"], x["processed_text"]), axis=1)
+
+    # Calculate word2vec similarities between the given title's processed_text and the processed_text of each row in result_df
+    title_vector = get_avg_vector(title_row["processed_text"].iloc[0], model).reshape(1, -1)
+    result_df["similarity"] = result_df["processed_text"].apply(lambda x: cosine_similarity(title_vector, get_avg_vector(x, model).reshape(1, -1)).item())
+
+    # Sort the DataFrame based on similarity values in descending order
     result_df.sort_values(by=["similarity"], ascending=False, inplace=True)
-    return result_df[['title', 'similarity']].head(n)
+
+    return result_df[['title', 'type', 'similarity']].head(n)
 
 
 def _get_cosine_similarity(title: str, model: typing.Any, data: pd.DataFrame, n: int = 10) -> pd.DataFrame:
